26f53fed059fc120319308237e4720f03b2743d1
Lubchyk <lubchyk@yandex.ua>
2017-01-03 22:10:03 +0200

    1

--- src/com/javarush/test/level03/lesson12/home02/Solution.java
+++ src/com/javarush/test/level03/lesson12/home02/Solution.java
@@ -16,40 +16,41 @@ public class Solution
 //        }
 //        while (i < 10);
 
-//        Scanner scanner = new Scanner(System.in);
-//        int day = scanner.nextInt();
-//        int a = 0;
-//        switch (day) {
-//            case 1 :
-//                System.out.print("понедельник");
-//                a++;
-//                break;
-//            case 2 :
-//                System.out.print("вторник");
-//                a++;
-//                break;
-//            case 3 :
-//                System.out.print("среда");
-//                a++;
-//                break;
-//            case 4 :
-//                System.out.print("четверг");
-//                a++;
-//                break;
-//            case 5 :
-//                System.out.print("пятница");
-//                a++;
-//                break;
-//            case 6 :
-//                System.out.print("суббота");
-//                a++;
-//                break;
-//            case 7 :
-//                System.out.print("воскресенье");
-//                a++;
-//                break;
-//                    }
-//        if(a > 0 && a <= 2 || a == 2) System.out.print("такого дня недели не существует");
+        Scanner scanner = new Scanner(System.in);
+        int day = scanner.nextInt();
+        int a = 0;
+        switch (day) {
+            case 1 :
+                System.out.print("понедельник");
+                a++;
+                break;
+            case 2 :
+                System.out.print("вторник");
+                a++;
+                break;
+            case 3 :
+                System.out.print("среда");
+                a++;
+                break;
+            case 4 :
+                System.out.print("четверг");
+                a++;
+                break;
+            case 5 :
+                System.out.print("пятница");
+                a++;
+                break;
+            case 6 :
+                System.out.print("суббота");
+                a++;
+                break;
+            case 7 :
+                System.out.print("воскресенье");
+                a++;
+                break;
+                    }
+        if(a <= 0 || a > 7) System.out.print("такого дня недели не существует");
+        //if(a > 0 && a <= 2 || a == 2) System.out.print("такого дня недели не существует");
 
     }
 
