3a74fcd793d37d4731715fd164f68b3de0450165
Lubchyk <lubchyk@yandex.ua>
2017-02-01 00:14:49 +0200

    Add new task

--- src/com/javarush/test/level08/lesson11/home05/Solution.java
+++ src/com/javarush/test/level08/lesson11/home05/Solution.java
@@ -3,6 +3,8 @@ package com.javarush.test.level08.lesson11.home05;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 
 /* Мама Мыла Раму. Теперь с большой буквы
 Написать программу, которая вводит с клавиатуры строку текста.
@@ -18,8 +20,25 @@ import java.io.InputStreamReader;
 
 public class Solution {
     public static void main(String[] args) throws IOException {
+        List<String> space = new ArrayList<String>();
+        String a = "";
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         String s = reader.readLine();
+        char[] arr = s.toCharArray();
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == ' ') {
+                for (int j = i; j < arr.length; j++) {
+                    if (arr[i] != ' ') break;
+                    a = a + " ";
+                    i++;
+                }
+            }
+            if (!a.equals("")) {
+                space.add(a);
+                a = "";
+            }
+        }
+        s = s.trim();
         String s1[] = s.split("\\s+"); // розбиває строку на декілька
         String bk1 = s1[0].substring(0, 1); // копіює вказаний символ із строки
         String bk2 = s1[1].substring(0, 1); // копіює вказаний символ із строки
@@ -30,6 +49,31 @@ public class Solution {
         bk1 = bk1.toUpperCase();
         bk2 = bk2.toUpperCase();
         bk3 = bk3.toUpperCase();
-        System.out.println(bk1 + sl1 + " " + bk2 + sl2 + " " + bk3 + sl3);
+        if (space.size() == 2) System.out.println(bk1 + sl1 + space.get(0) + bk2 + sl2 + space.get(1) + bk3 + sl3);
+        if (space.size() == 3)
+            System.out.println(space.get(0) + bk1 + sl1 + space.get(1) + bk2 + sl2 + space.get(2) + bk3 + sl3);
+        if (space.size() == 4)
+            System.out.println(space.get(0) + bk1 + sl1 + space.get(1) + bk2 + sl2 + space.get(2) + bk3 + sl3 + space.get(3));
+    }
+
+    public static void method2() throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String s = reader.readLine();
+
+        boolean upDone = false; // если увеличили первый символ слова
+
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == ' ' && upDone) {
+                upDone = false;
+                continue;
+            }
+            if (s.charAt(i) == ' ')
+                continue;
+            if (s.charAt(i) != ' ' && !upDone) {
+                s = s.substring(0, i) + ((s.substring(i, i + 1)).toUpperCase()) + s.substring(i + 1);
+                upDone = true;
+            }
+        }
+        System.out.println(s);
     }
 }
--- src/com/javarush/test/level08/lesson11/home09/Solution.java
+++ src/com/javarush/test/level08/lesson11/home09/Solution.java
@@ -14,57 +14,39 @@ JANUARY 2 2020 = false
 
 public class Solution {
     public static void main(String[] args) {
-        isDateOdd("JANUARY 1 2013");
-
-        Date yearStartTime = new Date();
-        yearStartTime.setHours(0);
-        yearStartTime.setMinutes(0);
-        yearStartTime.setSeconds(0);
-
-        yearStartTime.setDate(1);      // первое число
-        yearStartTime.setMonth(0);     // месяц январь, нумерация для месяцев 0-11
-
-        Date currentTime = new Date();
-        long msTimeDistance = currentTime.getTime() - yearStartTime.getTime();
-        long msDay = 24 * 60 * 60 * 1000;  //сколько миллисекунд в одних сутках
-
-        int dayCount = (int) (msTimeDistance/msDay); //количество целых дней
-       // System.out.println("Days from start of year: " + dayCount);
-
+        isDateOdd("JANUARY 2 2020");
     }
 
     public static boolean isDateOdd(String date) {
-        long c =  31622400000L ;
-        Date oneYear = new Date();
-        oneYear.setTime(c);
-
-//        long f = date11.getTime() - date22.getTime();
-//        long a = date11.getTime();
-//        long b = date22.getTime();
-        int p = 147 - (2^13) + 2^17 + 4625*8 + 262;
-
+        long msDay = 24 * 60 * 60 * 1000;
 
         Date yearBigan = new Date(date);
 
+        long nextYaer = yearBigan.getYear();
+        int year = (int) nextYaer;
 
+        Date yearEnd = new Date(date);
+        yearEnd.setHours(0);
+        yearEnd.setMinutes(0);
+        yearEnd.setSeconds(0);
+        yearEnd.setDate(0);
+        yearEnd.setMonth(0);
+        yearEnd.setYear(year);
 
-        Date year = new Date();
-        year.setHours(0);
-        year.setMinutes(0);
-        year.setSeconds(0);
-        year.setDate(1);
-        year.setMonth(0);
-        year.setYear(1);
+        long year1 = yearBigan.getTime();
+        long year2 = yearEnd.getTime();
+        long period = year1 - year2;
 
-        long year1 = yearBigan.getYear();
-        year1 = year1 + c;
-        //long g =
-        long msDay = 24 * 60 * 60 * 1000;
-        //long date18 = yearBigan.getTime();
-       int dayCount = (int) (year1 / msDay);
-        System.out.println(p);
+        int dayCount = ((int) (period / msDay));
 
-
-        return true;
+        System.out.println(dayCount);
+        boolean resalt = update(dayCount);
+        System.out.println(resalt);
+        return resalt;
+    }
+    static boolean update(int a) {
+        int b = a % 2;
+        if (b == 1) return true;
+        else return false;
     }
 }
